42ed6a6e195bb4f18453a56128c68adbb8371a7d
Yukti Mehta <mehtayukti317@gmail.com>
2026-01-23 18:22:50 +0530

    Add live url

--- helper/axios.js
+++ helper/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-const BASE_URL = "http://localhost:5000/api";
-// const BASE_URL = import.meta.env.VITE_API_BASE_URL ;
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://pharmaconnect-backend-ywps.onrender.com" ;
 
 const service = axios.create({
   baseURL: BASE_URL,
